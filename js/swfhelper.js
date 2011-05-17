5b162f61419482b41faeadffd223d5ff57142db8
flash gallery <flash gallery@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-17 13:58:46 +0000

    1.2.4
    git-svn-id: https://plugins.svn.wordpress.org/1-flash-gallery/tags/1.2.4@385851 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/swfhelper.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function(){
+	jQuery('.fgallery_message').html('<a href="http://1plugin.com" target="_blank">Photo Gallery Wordpress Plugin<\/a>').css('font-size','8px').css('line-height','12px').css('text-align','center');
+});
